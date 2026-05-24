da01c285ee6faadaf827387d56cc775a08dcb128
Adarsh2709 <jhaadarsh709@gmail.com>
2026-05-25 02:15:40 +0530

    feat : Config application and added Entity

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+}
